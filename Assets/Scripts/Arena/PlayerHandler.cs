614c3066a11a3ef7361006102fd2dd178336b5c1
TheKaoser <kaosermail@gmail.com>
2020-02-01 14:40:02 +0100

    reverting changes

--- Assets/Scripts/Arena/PlayerHandler.cs
+++ Assets/Scripts/Arena/PlayerHandler.cs
@@ -12,19 +12,44 @@ public class PlayerHandler : MonoBehaviour
     public float speed;
     Vector2 moveDirection;
     Vector2 aimDirection;
+    bool toolNear;
+    bool brokenNear;
+    bool hasTool;
+    float takingToolTimer = 3;
+    float repairingTimer = 3;
 
+    private void Start()
+    {
+        this.transform.position += new Vector3(moveDirection.x,0, moveDirection.y) * speed *  Time.deltaTime;
 
-// alante 0, 1 - atras 0, -1 izquierda -1 0, der 1 0
-    // private void Start()
-    // {
-    //     this.transform.position += new Vector3(moveDirection.x, 0, moveDirection.y) * speed *  Time.deltaTime;
+        if (toolNear && Input.GetKey(KeyCode.E))
+        {
+            takingToolTimer -= Time.deltaTime;
+            if (takingToolTimer < 0)
+            {
+                hasTool = true;
+            }
+        }
+        else
+        {
+            takingToolTimer = 3;
+        }
 
-    //     this.transform.position -= new Vector3(0, 0, distanceFromPlanet);
-    // }
+        if (brokenNear && hasTool && Input.GetKeyDown(KeyCode.E))
+        {
+            repairingTimer -= Time.deltaTime;
+            if (repairingTimer < 0)
+            {
+                
+            }
+        }
+
+        this.transform.position -= new Vector3(0, 0, distanceFromPlanet);
+    }
 
     private void Update()
     {
-        planetCenter.transform.Rotate(new Vector3(-moveDirection.y, -moveDirection.x, 0));
+        planetCenter.transform.Rotate(new Vector3(moveDirection.y, moveDirection.x, 0));
     }
 
     private void OnMove(InputValue value)
@@ -34,11 +59,35 @@ public class PlayerHandler : MonoBehaviour
 
     private void OnFire()
     {
-
+        Debug.Log("Fire");
     }
 
     public void OnCancel()
     {
-        
+        Debug.Log("Test");
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.CompareTag("Tool"))
+        {
+            toolNear = true;
+        } 
+        else if (other.CompareTag("Broken"))
+        {
+            brokenNear = true;
+        }
+    }
+    
+    void OnTriggerExit (Collider other)
+    {
+        if (other.CompareTag("Tool"))
+        {
+            toolNear = false;
+        }
+        else if (other.CompareTag("Broken"))
+        {
+            brokenNear = false;
+        }
     }
 }
--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -6,7 +6,6 @@ public class Item : MonoBehaviour
 {
     public bool hasBeenDestroyed;
     public float timeLeftToRepair = 20;
-    public GameObject gameManager;
 
     // Start is called before the first frame update
     void Start()
@@ -20,10 +19,9 @@ public class Item : MonoBehaviour
         if (hasBeenDestroyed)
         {
             timeLeftToRepair -= Time.deltaTime;
-            gameManager.GetComponent<GameManager>().worldPopulation -= 1;
             if (timeLeftToRepair < 0)
             {
-                gameManager.GetComponent<GameManager>().worldPopulation -= 1000;
+                
             }
         }
     }
