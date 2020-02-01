e4100037e15f68819abbf487592baee418988071
TheKaoser <kaosermail@gmail.com>
2020-02-01 15:06:48 +0100

    new version

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -6,6 +6,7 @@ public class Item : MonoBehaviour
 {
     public bool hasBeenDestroyed;
     public float timeLeftToRepair = 20;
+    public GameObject gameManager;
 
     // Start is called before the first frame update
     void Start()
@@ -19,9 +20,10 @@ public class Item : MonoBehaviour
         if (hasBeenDestroyed)
         {
             timeLeftToRepair -= Time.deltaTime;
+            gameManager.GetComponent<GameManager>().worldPopulation -= 1;
             if (timeLeftToRepair < 0)
             {
-                
+                gameManager.GetComponent<GameManager>().worldPopulation -= 1000;
             }
         }
     }
