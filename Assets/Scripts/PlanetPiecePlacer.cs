ca576fd2787f13cc23eda7509564c78591f9742f
Michel <michelvanderlinden@live.nl>
2020-02-01 15:08:37 +0100

    Sgsah

--- Assets/Scripts/PlanetPiecePlacer.cs
+++ Assets/Scripts/PlanetPiecePlacer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlanetPiecePlacer : MonoBehaviour
 {
-    public GameObject planetPiecePrefab;
+    public List<GameObject> planetPiecePrefabs;
     public float pieceScale = 0.2f;
 
     public float yOffset = 0;
@@ -14,7 +14,7 @@ public class PlanetPiecePlacer : MonoBehaviour
     public void Start()
     {
         this.transform.Rotate(0, 0, 90);
-        piece = Instantiate(planetPiecePrefab, this.transform.position,this.transform.rotation) as  GameObject;
+        piece = Instantiate(planetPiecePrefabs[Random.Range(0,planetPiecePrefabs.Count)], this.transform.position,this.transform.rotation) as  GameObject;
         piece.transform.localScale = new Vector3(pieceScale, pieceScale, pieceScale);
     }
 
