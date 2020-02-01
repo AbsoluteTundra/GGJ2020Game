e1afc4194ba551921f83a1ec55e20cf71ab75dc2
Michel <michelvanderlinden@live.nl>
2020-02-01 17:03:44 +0100

    Planet Prefab

--- Assets/Scripts/PlanetPiecePlacer.cs
+++ Assets/Scripts/PlanetPiecePlacer.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class PlanetPiecePlacer : MonoBehaviour
 {
-    public GameObject planetPiecePrefab;
     public List<GameObject> planetPiecePrefabs;
     public float pieceScale = 0.2f;
 
     public float yOffset = 0;
 
+    GameObject piece = null;
+
     public void Start()
     {
         this.transform.Rotate(0, 0, 90);
-        GameObject piece = Instantiate(planetPiecePrefabs[Random.Range(0, planetPiecePrefabs.Count)], this.transform.position, this.transform.rotation) as GameObject;
+        piece = Instantiate(planetPiecePrefabs[Random.Range(0, planetPiecePrefabs.Count)], this.transform.position,this.transform.rotation) as GameObject;
         piece.transform.localScale = new Vector3(pieceScale, pieceScale, pieceScale);
+    }
+
+    public void Update()
+    {
+        piece.transform.position = this.transform.position;
+        piece.transform.rotation = this.transform.rotation;
     }
 }
