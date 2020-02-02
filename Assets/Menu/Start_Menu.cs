1c8aad6aeef183c7c2f177fa2aa8ff91ab334721
Giandomenico Lombardi <gmilh13@gmail.com>
2020-02-02 12:03:12 +0100

    Start Screen
    Created UI prefab for the start screen

---
+++ Assets/Menu/Start_Menu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Start_Menu : MonoBehaviour
+{
+    public Canvas hud;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hud.gameObject.SetActive(false);
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        Time.timeScale = 1;
+        gameObject.SetActive(false);
+        hud.gameObject.SetActive(true);
+    }
+
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
+}
